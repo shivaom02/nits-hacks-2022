ead8b9d6443a1a77e43a477cde07ec9daa8d4d12
Trishanku Sarma <trishankusarma165@gmail.com>
2022-10-22 02:19:20 +0530

    mid push redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {toast} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 import './App.css';
+
+export const Toastify=(type,msg)=>{
+  
+  switch(type){
+    case "success":
+      toast.success(msg);
+      break;
+    case "warning":
+      toast.warn(msg);
+      break;
+    case "error":
+      toast.error(msg);
+      break;
+    case "info":
+      toast.info(msg);
+      break;     
+  }
+  
+}
 
 const Team = React.lazy( () => import("./pages/Team" ) );
 const Home = React.lazy( () => import("./pages/Home" ) );
